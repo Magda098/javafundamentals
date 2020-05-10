770a04b2424ddc28449044d77f03e5b90868bb21
Mag Meg <64429234+Magda098@users.noreply.github.com>
2020-05-10 19:50:39 +0300

    adds java classes example

--- src/main.java
+++ src/main.java
@@ -1,22 +1,12 @@
 public class main {
-    public static double calculateEmployeeGrossSalary(double hoursPerWeek,double earningsPerHour,
-                                                      int vacationDays) {
-        /* if (hoursPerWeek < 0);{
-            return -1;
-        }
-       if (earningsPerHour < 0);{
-            return -1;
-        }*/
-        double weeklyPaycheck = hoursPerWeek*earningsPerHour;
-        double unpaidTime=vacationDays*earningsPerHour*8;
-        return (weeklyPaycheck*52)-unpaidTime;
-
-    }
-
     public static void main(String[] args) {
+        Triangle triangleA=new Triangle(15,8,15,8,17);
+        Triangle triangleB=new Triangle(3,2.598,3,3,3);
 
-        double grossSalary=calculateEmployeeGrossSalary(40,15,8);
-        System.out.println(grossSalary);
+    double triangleAArea=triangleA.findArea();
+    System.out.println(triangleAArea);
+    double triangleBArea=triangleB.findArea();
+    System.out.println(triangleBArea);
     }
     
 }
